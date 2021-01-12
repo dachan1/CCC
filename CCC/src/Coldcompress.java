a6155ad2c4e4c5ea5d57b89c2604568c9167805d
danielchan <danielchan@192.168.0.166>
2021-01-12 13:43:16 -0500

    Mardi 12 janvier 2021 J3 Coldcompress

--- CCC/src/Coldcompress.java
+++ CCC/src/Coldcompress.java
@@ -7,16 +7,19 @@ import java.util.Scanner;
 public class Coldcompress
 	{
 
-		public static void main(String[] args)
-			{
-				Scanner sc=new Scanner(System.in);
-				int x=sc.nextInt();
-				int same=0;
-				int low=0;
-				for(int i=0; i<x; i++) 
-				{
-
-			}
-
-	}
+		public static void main(String [] args) {
+		        Scanner in = new Scanner(System.in);
+		        int n = in.nextInt(); in.nextLine();
+		        for (int k=1; k<=n; k++) {
+		            String symbol = in.nextLine();
+		            for(int i=0, j=0; i<symbol.length(); ) {
+		                while(j < symbol.length() && symbol.charAt(i) == symbol.charAt(j)) {
+		                    j++;
+		                }
+		                System.out.print((j-i) + " " + symbol.charAt(i) + " ");
+		                i = j;
+		            }
+		            System.out.println();
+		        }
+		    }
 	}
